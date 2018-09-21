0332f90512197f9ed28f32e87b5713101555f5c8
Jeremy Taylor <jt9916@gmail.com>
2018-09-20 20:55:51 -0400

    yuh

---
+++ WindowsProject/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 20, 2018
+ *      Author: jt991
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+	return 0;
+}
+
+
